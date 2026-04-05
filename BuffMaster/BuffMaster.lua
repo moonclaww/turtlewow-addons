fb4c38cb5ab99870786c75e0ef81e60b5f74ff40
moonclaw <me@moonclaw.org>
2026-04-04 22:36:00 -0230

    BufferMaster: Add timer to debuff

--- BuffMaster/BuffMaster.lua
+++ BuffMaster/BuffMaster.lua
@@ -12,11 +12,289 @@ local BuffMaster_Defaults = {
 }
 
 -- Maximum number of buff/debuff buttons to track
-local BUFF_BUTTON_COUNT = 24
+local BUFF_BUTTON_COUNT = 32
 local DEBUFF_BUTTON_COUNT = 16
+local BUFFS_PER_ROW = 8
+local DEBUFF_BUTTON_START = 32
+
+-- Visual spacing for the player's aura rows
+local BUFF_ROW2_BASE_OFFSET = -5
+local BUFF_ROW3_BASE_OFFSET = -70
+local TIMER_ROW_CLEARANCE = 7
+local DEBUFF_ROW_CLEARANCE = 5
+local TIMER_LABEL_OFFSET_Y = 0
+local TIMER_LABEL_WIDTH_PADDING = 8
+local TIMER_LABEL_HEIGHT = 12
+local BUFF_WARNING_THRESHOLD = BUFF_WARNING_TIME or 30
+
+local BuffMaster_AuraConfigs = {
+    Buff = {
+        frameName = "BuffMasterFrame",
+        buttonPrefix = "BuffButton",
+        buttonStart = 0,
+        count = BUFF_BUTTON_COUNT,
+        filter = "HELPFUL",
+        permanentColor = {0.0, 1.0, 0.0},
+        warningColor = {1.0, 0.0, 0.0},
+        normalColor = {0.0, 1.0, 0.0}
+    },
+    Debuff = {
+        frameName = "BuffMasterDebuffFrame",
+        buttonPrefix = "BuffButton",
+        buttonStart = DEBUFF_BUTTON_START,
+        count = DEBUFF_BUTTON_COUNT,
+        filter = "HARMFUL",
+        permanentColor = {1.0, 0.5, 0.0},
+        warningColor = {1.0, 1.0, 0.0},
+        normalColor = {1.0, 0.0, 0.0}
+    }
+}
 
 -- Saved settings (loaded from SavedVariables)
 BuffMaster_SavedSettings = nil
+
+local BuffMaster_AuraTooltip
+local BuffMaster_AuraTooltipLines = {}
+local BuffMaster_PatternMin
+local BuffMaster_PatternSec
+
+local function BuffMaster_GetAuraTooltip()
+    if BuffMaster_AuraTooltip then
+        return BuffMaster_AuraTooltip
+    end
+
+    local tooltip = CreateFrame("GameTooltip", "BuffMasterAuraTooltip", WorldFrame)
+    tooltip:SetOwner(WorldFrame, "ANCHOR_NONE")
+
+    for index = 1, 8, 1 do
+        local left = tooltip:CreateFontString("BuffMasterAuraTooltipTextLeft" .. index, nil, "GameFontNormal")
+        local right = tooltip:CreateFontString("BuffMasterAuraTooltipTextRight" .. index, nil, "GameFontNormal")
+        tooltip:AddFontStrings(left, right)
+        BuffMaster_AuraTooltipLines[index] = left
+    end
+
+    BuffMaster_AuraTooltip = tooltip
+    return tooltip
+end
+
+local function BuffMaster_ParseTooltipTimeLeft(auraIndex)
+    if not auraIndex or auraIndex == -1 then
+        return nil
+    end
+
+    if not BuffMaster_PatternMin then
+        BuffMaster_PatternMin = BuffMaster_GetLocale("PATTERN_MIN")
+        BuffMaster_PatternSec = BuffMaster_GetLocale("PATTERN_SEC")
+    end
+
+    local tooltip = BuffMaster_GetAuraTooltip()
+    tooltip:ClearLines()
+    tooltip:SetOwner(WorldFrame, "ANCHOR_NONE")
+    tooltip:SetPlayerBuff(auraIndex)
+
+    for index = 1, 8, 1 do
+        local line = BuffMaster_AuraTooltipLines[index]
+        local text = line and line:GetText()
+        if text then
+            local _, _, _, minutes = string.find(text, BuffMaster_PatternMin)
+            if minutes then
+                return tonumber(minutes) * 60
+            end
+
+            local _, _, _, seconds = string.find(text, BuffMaster_PatternSec)
+            if seconds then
+                return tonumber(seconds)
+            end
+        end
+    end
+end
+
+local function BuffMaster_GetAuraLabel(config, index, button)
+    if button and button.BuffMasterLabel then
+        return button.BuffMasterLabel
+    end
+
+    local parent = button or getglobal(config.frameName)
+    if not parent then
+        return nil
+    end
+
+    local label = parent:CreateFontString(nil, "OVERLAY", "GameFontHighlightSmall")
+    label:SetJustifyH("CENTER")
+    label:SetHeight(TIMER_LABEL_HEIGHT)
+    label:Hide()
+
+    if button then
+        button.BuffMasterLabel = label
+    end
+
+    return label
+end
+
+local function BuffMaster_AnchorAuraLabel(label, button)
+    if label.anchorButton ~= button then
+        label:ClearAllPoints()
+        label:SetPoint("TOP", button, "BOTTOM", 0, TIMER_LABEL_OFFSET_Y)
+        label.anchorButton = button
+    end
+
+    label:SetWidth(button:GetWidth() + TIMER_LABEL_WIDTH_PADDING)
+end
+
+local function BuffMaster_GetAuraButtonByIndex(prefix, index)
+    return getglobal(prefix .. index)
+end
+
+local function BuffMaster_GetAuraButton(config, slotIndex)
+    local buttonIndex = (config.buttonStart or 0) + slotIndex
+    return BuffMaster_GetAuraButtonByIndex(config.buttonPrefix, buttonIndex), buttonIndex
+end
+
+local function BuffMaster_GetAuraTimeLeftFromIndex(config, auraIndex)
+    if auraIndex == nil or auraIndex == -1 then
+        return nil
+    end
+
+    local timeLeft = GetPlayerBuffTimeLeft(auraIndex)
+    if (timeLeft == nil or timeLeft == 0) and config.filter == "HARMFUL" then
+        local tooltipTime = BuffMaster_ParseTooltipTimeLeft(auraIndex)
+        if tooltipTime ~= nil then
+            return tooltipTime
+        end
+    end
+
+    return timeLeft
+end
+
+local function BuffMaster_GetAuraTimeLeft(config, button, slotIndex, buttonIndex)
+    local timeLeft = BuffMaster_GetAuraTimeLeftFromIndex(config, button.buffIndex)
+    if timeLeft ~= nil then
+        return timeLeft
+    end
+
+    local auraIndex = GetPlayerBuff(slotIndex, config.filter)
+    timeLeft = BuffMaster_GetAuraTimeLeftFromIndex(config, auraIndex)
+    if timeLeft ~= nil then
+        return timeLeft
+    end
+
+    local buttonId = button.GetID and button:GetID()
+    if buttonId and buttonIndex ~= nil then
+        local baseIndex = buttonIndex - slotIndex
+        local slotFromId = buttonId - baseIndex
+        if slotFromId ~= slotIndex then
+            auraIndex = GetPlayerBuff(slotFromId, config.filter)
+            timeLeft = BuffMaster_GetAuraTimeLeftFromIndex(config, auraIndex)
+            if timeLeft ~= nil then
+                return timeLeft
+            end
+        end
+    end
+end
+
+local function BuffMaster_SetLabelColor(label, color)
+    label:SetTextColor(color[1], color[2], color[3])
+end
+
+local function BuffMaster_HideAuraLabels(config)
+    for slotIndex = 0, config.count - 1, 1 do
+        local button = BuffMaster_GetAuraButton(config, slotIndex)
+        local label = button and button.BuffMasterLabel
+        if label then
+            label:Hide()
+        end
+    end
+end
+
+local function BuffMaster_UpdateAuraTimers(config)
+    for slotIndex = 0, config.count - 1, 1 do
+        local button, buttonIndex = BuffMaster_GetAuraButton(config, slotIndex)
+        local label = BuffMaster_GetAuraLabel(config, slotIndex, button)
+
+        if label and button and button:IsVisible() then
+            local timeLeft = BuffMaster_GetAuraTimeLeft(config, button, slotIndex, buttonIndex)
+
+            if timeLeft ~= nil then
+                BuffMaster_AnchorAuraLabel(label, button)
+                label:SetAlpha(button:GetAlpha())
+                label:Show()
+
+                if timeLeft == 0 then
+                    BuffMaster_SetLabelColor(label, config.permanentColor)
+                    label:SetText("N/A")
+                elseif timeLeft < BUFF_WARNING_THRESHOLD then
+                    BuffMaster_SetLabelColor(label, config.warningColor)
+                    label:SetText(BuffMaster_FormatTime(timeLeft))
+                else
+                    BuffMaster_SetLabelColor(label, config.normalColor)
+                    label:SetText(BuffMaster_FormatTime(timeLeft))
+                end
+            else
+                label:Hide()
+            end
+        elseif label then
+            label:Hide()
+        end
+    end
+end
+
+local function BuffMaster_SetAuraRowPosition(frameName, point, relativeTo, relativePoint, xOffset, yOffset)
+    local frame = getglobal(frameName)
+    local relativeFrame = getglobal(relativeTo)
+
+    if frame and relativeFrame then
+        frame:ClearAllPoints()
+        frame:SetPoint(point, relativeFrame, relativePoint, xOffset, yOffset)
+    end
+end
+
+local function BuffMaster_FindLastVisibleBuffRowAnchor()
+    for slotIndex = BuffMaster_AuraConfigs.Buff.count - 1, 0, -1 do
+        local button, buttonIndex = BuffMaster_GetAuraButton(BuffMaster_AuraConfigs.Buff, slotIndex)
+        if button and button:IsVisible() then
+            local rowStartIndex = buttonIndex - math.mod(buttonIndex, BUFFS_PER_ROW)
+            return BuffMaster_GetAuraButtonByIndex("BuffButton", rowStartIndex)
+        end
+    end
+
+    local tempEnchant = getglobal("TempEnchant1")
+    if tempEnchant and tempEnchant:IsVisible() then
+        return tempEnchant
+    end
+
+    local tempEnchantFrame = getglobal("TemporaryEnchantFrame")
+    if tempEnchantFrame and tempEnchantFrame:IsVisible() then
+        return tempEnchantFrame
+    end
+end
+
+local function BuffMaster_UpdateDebuffPositions()
+    local firstDebuffButton
+    for slotIndex = 0, DEBUFF_BUTTON_COUNT - 1, 1 do
+        local button = BuffMaster_GetAuraButton(BuffMaster_AuraConfigs.Debuff, slotIndex)
+        if button and button:IsVisible() then
+            firstDebuffButton = button
+            break
+        end
+    end
+
+    if not firstDebuffButton then
+        return
+    end
+
+    local anchorButton = BuffMaster_FindLastVisibleBuffRowAnchor()
+    if not anchorButton then
+        return
+    end
+
+    firstDebuffButton:ClearAllPoints()
+    local yOffset = -DEBUFF_ROW_CLEARANCE
+    local anchorLabel = anchorButton.BuffMasterLabel
+    if anchorLabel and anchorLabel:IsShown() then
+        yOffset = -(TIMER_LABEL_HEIGHT + DEBUFF_ROW_CLEARANCE)
+    end
+    firstDebuffButton:SetPoint("TOPRIGHT", anchorButton, "BOTTOMRIGHT", 0, yOffset)
+end
 
 -- OnLoad handler for the config frame
 function BuffMasterConfigFrame_OnLoad()
@@ -46,6 +324,7 @@ function BuffMasterConfigFrame_OnEvent(event)
             BuffMasterFrame:Show()
         else
             BuffMasterFrame:Hide()
+            BuffMaster_HideAuraLabels(BuffMaster_AuraConfigs.Buff)
         end
 
         -- Apply settings - show/hide item buff bar
@@ -60,6 +339,7 @@ function BuffMasterConfigFrame_OnEvent(event)
             BuffMasterDebuffFrame:Show()
         else
             BuffMasterDebuffFrame:Hide()
+            BuffMaster_HideAuraLabels(BuffMaster_AuraConfigs.Debuff)
         end
     end
 
@@ -137,6 +417,7 @@ function BuffMasterConfigFrame_Okay()
     else
         BuffMaster_SavedSettings.BuffMaster_Enable = "0"
         BuffMasterFrame:Hide()
+        BuffMaster_HideAuraLabels(BuffMaster_AuraConfigs.Buff)
     end
 
     -- Save ItemBuff enable state
@@ -155,6 +436,7 @@ function BuffMasterConfigFrame_Okay()
     else
         BuffMaster_SavedSettings.Debuff_Enable = "0"
         BuffMasterDebuffFrame:Hide()
+        BuffMaster_HideAuraLabels(BuffMaster_AuraConfigs.Debuff)
     end
 
     HideUIPanel(parent)
@@ -163,81 +445,35 @@ end
 
 -- Update buff timer labels on each frame update
 function BuffMaster_UpdateTimers()
-    for index = 0, BUFF_BUTTON_COUNT - 1, 1 do
-        local buffButton = getglobal("BuffButton" .. index)
-        local buffLabel = getglobal("BuffLabel" .. index)
+    BuffMaster_UpdateAuraTimers(BuffMaster_AuraConfigs.Buff)
 
-        if buffButton:IsVisible() then
-            buffLabel:Show()
-            local buffIndex = buffButton.buffIndex
-            local timeLeft = GetPlayerBuffTimeLeft(buffIndex)
-
-            if timeLeft == 0 then
-                -- No duration (permanent buff or unknown)
-                buffLabel:SetTextColor(0.0, 1.0, 0.0)
-                buffLabel:SetText("N/A")
-            elseif timeLeft < BUFF_WARNING_TIME then
-                -- Warning - buff about to expire (red)
-                buffLabel:SetTextColor(1.0, 0.0, 0.0)
-                buffLabel:SetText(BuffMaster_FormatTime(timeLeft))
-                buffLabel:SetAlpha(buffButton:GetAlpha())
-            else
-                -- Normal duration (green)
-                buffLabel:SetTextColor(0.0, 1.0, 0.0)
-                buffLabel:SetText(BuffMaster_FormatTime(timeLeft))
-            end
-        else
-            buffLabel:Hide()
-        end
+    if BuffMaster_SavedSettings and BuffMaster_SavedSettings.Debuff_Enable == "1" then
+        BuffMaster_UpdateDebuffPositions()
+        BuffMaster_UpdateAuraTimers(BuffMaster_AuraConfigs.Debuff)
     end
 end
 
 -- Update debuff timer labels on each frame update
 function BuffMaster_UpdateDebuffTimers()
-    for index = 0, DEBUFF_BUTTON_COUNT - 1, 1 do
-        local debuffButton = getglobal("DebuffButton" .. index)
-        local debuffLabel = getglobal("DebuffLabel" .. index)
-
-        if debuffButton and debuffButton:IsVisible() then
-            debuffLabel:Show()
-            local buffIndex = debuffButton.buffIndex
-            local timeLeft = GetPlayerBuffTimeLeft(buffIndex)
-
-            if timeLeft == 0 then
-                -- No duration (permanent debuff or unknown)
-                debuffLabel:SetTextColor(1.0, 0.5, 0.0)
-                debuffLabel:SetText("N/A")
-            elseif timeLeft < BUFF_WARNING_TIME then
-                -- Debuff about to expire (yellow - good news!)
-                debuffLabel:SetTextColor(1.0, 1.0, 0.0)
-                debuffLabel:SetText(BuffMaster_FormatTime(timeLeft))
-                debuffLabel:SetAlpha(debuffButton:GetAlpha())
-            else
-                -- Normal duration (red for debuffs)
-                debuffLabel:SetTextColor(1.0, 0.0, 0.0)
-                debuffLabel:SetText(BuffMaster_FormatTime(timeLeft))
-            end
-        else
-            if debuffLabel then
-                debuffLabel:Hide()
-            end
-        end
+    if not BuffMaster_SavedSettings or BuffMaster_SavedSettings.BuffMaster_Enable ~= "1" then
+        BuffMaster_UpdateDebuffPositions()
+        BuffMaster_UpdateAuraTimers(BuffMaster_AuraConfigs.Debuff)
     end
 end
 
 -- Update buff button positions based on whether timers are shown
 function BuffButtons_UpdatePositions()
-    local showTimers = SHOW_BUFF_DURATIONS == "1" or 
-        (BuffMaster_SavedSettings and 
-         BuffMaster_SavedSettings.BuffMaster_Enable and 
+    local showTimers = SHOW_BUFF_DURATIONS == "1" or
+        (BuffMaster_SavedSettings and
+         BuffMaster_SavedSettings.BuffMaster_Enable and
          BuffMaster_SavedSettings.BuffMaster_Enable == "1")
 
-    if showTimers then
-        getglobal("BuffButton8"):SetPoint("TOP", "TempEnchant1", "BOTTOM", 0, -15)
-        getglobal("BuffButton16"):SetPoint("TOPRIGHT", "TemporaryEnchantFrame", "TOPRIGHT", 0, -90)
-    else
-        getglobal("BuffButton8"):SetPoint("TOP", "TempEnchant1", "BOTTOM", 0, -5)
-        getglobal("BuffButton16"):SetPoint("TOPRIGHT", "TemporaryEnchantFrame", "TOPRIGHT", 0, -70)
+    local timerClearance = showTimers and TIMER_ROW_CLEARANCE or 0
+
+    BuffMaster_SetAuraRowPosition("BuffButton8", "TOP", "TempEnchant1", "BOTTOM", 0, BUFF_ROW2_BASE_OFFSET - timerClearance)
+    BuffMaster_SetAuraRowPosition("BuffButton16", "TOPRIGHT", "TemporaryEnchantFrame", "TOPRIGHT", 0, BUFF_ROW3_BASE_OFFSET - (timerClearance * 2))
+    if not BuffMaster_SavedSettings or BuffMaster_SavedSettings.Debuff_Enable == "1" then
+        BuffMaster_UpdateDebuffPositions()
     end
 end
 
@@ -251,7 +487,7 @@ end
 -- Slash command handler
 SLASH_BUFFMASTER1 = "/buffmaster"
 SLASH_BUFFMASTER2 = "/bm"
-SlashCmdList["BUFFMASTER"] = function(msg)
+SlashCmdList["BUFFMASTER"] = function()
     ShowUIPanel(BuffMasterConfigFrame)
 end
 
